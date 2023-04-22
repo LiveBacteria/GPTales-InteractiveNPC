99dce8ef994ef9cfe170c6e865687b12252d7024
Tyler Poore <tylerpoore2000@gmail.com>
2023-04-22 16:07:13 -0700

    Added npcSpeak command - Precursor setup for VectorDB & LLM integration

--- Game/js/plugins/npcUtils.js
+++ Game/js/plugins/npcUtils.js
@@ -14,6 +14,12 @@
  * @desc The ID of the event to make speak.
  * @default 1
  *
+ * @arg message
+ * @type text
+ * @text Message
+ * @desc The message for the NPC to say.
+ * @default Hello, I'm an NPC!
+ *
  * @command spawnNPC
  * @text Spawn NPC
  * @desc Spawns an NPC with given parameters.
@@ -45,19 +51,25 @@
 
 (() => {
   // Use this to call the plugin commands from other plugins.
-  // PluginManager.callCommand(self, 'npcUtils', 'spawnNPC',[])
+  // PluginManager.callCommand(self, 'npcUtils', 'spawnNPC',{x: 1, y: 6, mapTarget: 1, eventId: 1})
 
   // Doesn't work at this time due to the way the plugin manager works.
   // Will likely need to call to an external reference for function calls instead.
   PluginManager.registerCommand("npcUtils", "npcSpeak", function (args) {
     const eventId = Number(args.eventId);
-    const npc = $dataMap.events[eventId];
-    if (npc && npc.speak) {
-      npc.speak();
+    const npcEvent = $gameMap.event(eventId);
+
+    if (npcEvent) {
+      // Determine message via Cardinal-LLM interaction in future branch, for now just use the message from the plugin command.
+      const message = npcEvent.GPTales.memory.backstory;
+
+      if (message !== undefined && message !== null && message !== "") {
+        $gameMessage.add(message);
+      } else {
+        console.warn(`NPC with event ID ${eventId} does not have a message.`);
+      }
     } else {
-      console.warn(
-        `NPC with event ID ${eventId} not found or doesn't have a speak function.`
-      );
+      console.warn(`NPC with event ID ${eventId} not found.`);
     }
   });
 
@@ -71,5 +83,61 @@
 
     // Utilises the GALV_EventSpawnerMZ plugin
     Galv.SPAWN.event(eventId, "xy", [x, y], "all", true, mapTarget);
+    appendNPCFunctions($gameMap._lastSpawnEventId);
+    // If needed: $gameMap._lastSpawnEventId
   });
 })();
+
+// Utility Funcitons
+
+// Generate NPC Event Data
+const generateNPCEventData = (npcData) => {
+  const { characterName, pageData, functionCalls } = npcData;
+};
+
+const appendNPCFunctions = (npcEventId) => {
+  const ref = $gameMap._events[npcEventId];
+  ref.speak = function (message) {
+    $gameMessage.add(message);
+  };
+
+  ref.GPTales = {};
+
+  ref.GPTales.memory = {};
+
+  ref.GPTales.memory.set = function (key, value) {
+    this[key] = value;
+  };
+
+  ref.GPTales.memory.get = function (key) {
+    return this[key];
+  };
+
+  ref.GPTales.memory.clear = function (key) {
+    this[key] = undefined;
+  };
+
+  ref.GPTales.memory.has = function (key) {
+    return this[key] !== undefined;
+  };
+
+  ref.GPTales.memory.keys = function () {
+    return Object.keys(this);
+  };
+
+  ref.GPTales.memory.values = function () {
+    return Object.values(this);
+  };
+
+  ref.GPTales.memory.backstory = "Hello traveler, I am an NPC.";
+
+  ref.GPTales.memory.setBackstory = function (backstory) {
+    this.backstory = backstory;
+  };
+
+  ref.GPTales.memory.getBackstory = function () {
+    return this.backstory;
+  };
+
+  ref.GPTales.memory.illuminations = [];
+};
