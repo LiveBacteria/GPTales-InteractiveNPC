61ee00e1bf6bb9afee1cd72b7545ac7aa5921ee5
Tyler Poore <tylerpoore2000@gmail.com>
2023-04-22 16:55:49 -0700

    Added Precursor setup for Cardinal System

---
+++ Game/js/plugins/GPTales.js
@@ -0,0 +1,19 @@
+// This file contains the function exports that interact and make calls to the Cardinal System.
+
+const fs = require("fs");
+const path = require("path");
+
+const { Cardinal } = require(".GPTales/Cardinal");
+
+const main = function () {
+  const context = fs.readFileSync(
+    path.join(__dirname, "GPTales/gameContext.json"),
+    "utf8"
+  );
+
+  const gameContext = JSON.parse(context);
+
+  const cardinal = new Cardinal(gameContext);
+
+  cardinal.start();
+};
